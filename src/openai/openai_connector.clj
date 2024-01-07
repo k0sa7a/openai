2a7f91908cd45d1b67e2b803d3245ae5abe8c163
Stefan Filev <stefan.s.filev@gmail.com>
2024-01-07 16:00:32 +0000

    added single message creation, run creation and listing of run steps functionality

--- src/openai/openai_connector.clj
+++ src/openai/openai_connector.clj
@@ -40,7 +40,7 @@
     (get parsed-body :id)))
 
 
-(def assistant-id (create-assistant "Assistant test1" "gpt-3.5-turbo-1106" "You will reply in rhymes"))
+(def assistant-id (create-assistant "Assistant test1" "gpt-3.5-turbo-1106" "Please return the ID, company name and generate your own description of the value for each in the format: ID - Value - Description; Do not return anything else"))
 (println "Assistant ID is:" assistant-id)
 
 ;; function to create a thread (no messages created for it)
@@ -56,3 +56,59 @@
 
 (def thread-id (create-thread))
 (println "Thread ID is:" thread-id)
+
+;; function to create a single message
+;; returns the id an message content
+(defn create-message [content]
+  (let [payload (json/generate-string {"role" "user"
+                                       "content" content})
+        response (client/post (str "https://api.openai.com/v1/threads/" thread-id "/messages")
+                              {:body  payload
+                               :headers {"Authorization" (str "Bearer " openai-api-key)
+                                         "Content-Type" "application/json"
+                                         "OpenAI-Beta" "assistants=v1"}})
+        body (:body response)
+        parsed-body (json/parse-string body true)]
+
+    (get parsed-body :id)))
+
+(create-message "1 - Feefo")
+(create-message "2 - MySQL")
+(create-message "3 - NetSuite")
+(create-message "42 - Mailchimp")
+
+
+;; create a run
+;; returns a run id
+(defn create-run []
+  (let [payload (json/generate-string {"assistant_id" assistant-id})
+        response (client/post (str "https://api.openai.com/v1/threads/" thread-id "/runs")
+                              {:body  payload
+                               :headers {"Authorization" (str "Bearer " openai-api-key)
+                                         "Content-Type" "application/json"
+                                         "OpenAI-Beta" "assistants=v1"}})
+        body (:body response)
+        parsed-body (json/parse-string body true)]
+
+    (get parsed-body :id)))
+
+(def run-id (create-run))
+(println "Run ID is:" run-id)
+
+;; list the run steps
+(defn list-run-steps []
+  (let [response (client/get (str "https://api.openai.com/v1/threads/" thread-id "/runs/" run-id "/steps")
+                             {:headers {"Authorization" (str "Bearer " openai-api-key)
+                                        "Content-Type" "application/json"
+                                        "OpenAI-Beta" "assistants=v1"}})
+        body (:body response)]
+    (println body))
+  )
+
+(list-run-steps)
+
+;; quick check on the run (to see if it failed)
+;; (client/get (str "https://api.openai.com/v1/threads/" thread-id "/runs/" run-id)
+;;             {:headers {"Authorization" (str "Bearer " openai-api-key)
+;;                        "Content-Type" "application/json"
+;;                        "OpenAI-Beta" "assistants=v1"}})
