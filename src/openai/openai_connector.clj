cd35fa097ae97674e8c3e61786e0929335d6013f
Stefan Filev <stefan.filev@kleene.ai>
2024-01-09 18:38:14 +0000

    clean up on file and run with multiple messages and single response msg

--- src/openai/openai_connector.clj
+++ src/openai/openai_connector.clj
@@ -40,8 +40,7 @@
     (get parsed-body :id)))
 
 
-(def assistant-id (create-assistant "Assistant test 2" "gpt-3.5-turbo-1106" "Please return the ID, company name and generate your own description of the value for each in the format: ID - Value - Description; Do not return anything else"))
-(println "Assistant ID is:" assistant-id)
+
 
 ;; function to create a thread (no messages created for it)
 ;; it returns the id of that thread
@@ -54,8 +53,7 @@
 
     (get parsed-body :id)))
 
-(def thread-id (create-thread))
-(println "Thread ID is:" thread-id)
+
 
 ;; function to create a single message
 ;; returns the id an message content
@@ -69,13 +67,15 @@
                                          "OpenAI-Beta" "assistants=v1"}})
         body (:body response)
         parsed-body (json/parse-string body true)]
-
+    (println parsed-body)
+    (println "---------------------------")
     (get parsed-body :id)))
 
-(create-message "1 - Feefo")
-(create-message "2 - MySQL")
-(create-message "3 - NetSuite")
-(create-message "42 - Mailchimp")
+;; old test with hard coded strings for the messages
+;; (create-message "1 - Feefo")
+;; (create-message "2 - MySQL")
+;; (create-message "3 - NetSuite")
+;; (create-message "42 - Mailchimp")
 
 
 ;; create a run
@@ -92,8 +92,7 @@
 
     (get parsed-body :id)))
 
-(def run-id (create-run))
-(println "Run ID is:" run-id)
+
 
 ;; list the run steps
 ;; (defn list-run-steps []
@@ -122,8 +121,7 @@
           (recur)) ; Retry the request
         (get-in parsed-body [:data 0 :step_details :message_creation :message_id])))))
 
-(def msg-id (list-run-steps))
-(println "Message ID is:" msg-id)
+
 ;; quick check on the run (to see if it failed)
 ;; (client/get (str "https://api.openai.com/v1/threads/" thread-id "/runs/" run-id)
 ;;             {:headers {"Authorization" (str "Bearer " openai-api-key)
@@ -159,7 +157,7 @@
 
 
 
-(println (retrieve-message))
+
 
 ;; delete the thread after run is done; prints body to check if deleted true
 (defn delete-thread []
@@ -172,7 +170,7 @@
 
     (println parsed-body)))
 
-(delete-thread)
+
 
 ;; delete the assistant after run is done; prints body to check if deleted true
 (defn delete-assistant []
@@ -185,4 +183,66 @@
 
     (println parsed-body)))
 
+
+
+;; parse the csv into a sequence of maps
+(defn parse-csv [file-path]
+  (with-open [reader (io/reader file-path)]
+    (let [rows (csv/read-csv reader)
+          headers (map keyword (first rows))]
+      (doall (map (fn [row] (zipmap headers row)) (rest rows))))))
+
+
+
+
+;; extract only the required columns and prepare sequence of strings to be used in message creation
+(defn extract-and-concat [data key1 key2]
+  (map #(str ((keyword key1) %) " - " ((keyword key2) %)) data))
+
+
+
+;; --- FLOW FOR THE OPEN AI TEST ---
+
+;; create an assistant
+(def assistant-id (create-assistant "Assistant test 2" "gpt-3.5-turbo-1106" "Please return the ID, company name and generate your own description of the value for each in the format: ID - Value - Description; Do not return anything else"))
+(println "Assistant ID is:" assistant-id)
+
+;; create a thread
+(def thread-id (create-thread))
+(println "Thread ID is:" thread-id)
+
+;; parse the csv with initial data
+(def data (parse-csv "src/openai/data.csv"))
+
+;; extract the columns that we require and concat the data
+(def sequence-for-messages (extract-and-concat data :FEATURE_ID :FEATURE_NAME))
+(prn sequence-for-messages)
+
+;; OPTION 1 (many messages and one run)..........|
+;; create the messages
+(doseq [msg sequence-for-messages]
+  (create-message msg)
+  (Thread/sleep 1000))
+
+;; create the run
+(def run-id (create-run))
+(println "Run ID is:" run-id)
+
+;; list run steps
+(def msg-id (list-run-steps))
+(println "Message ID is:" msg-id)
+
+;; retrieve the message
+(println (retrieve-message))
+;; ...............................................|
+
+
+;; OPTON 2 (many runs witn individual message)==========}
+
+
+
+;; delete thread
+;; (delete-thread)
+
+;; delete assistant
 (delete-assistant)
